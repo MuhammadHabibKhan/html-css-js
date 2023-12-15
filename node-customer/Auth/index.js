a74ec94d5afd31128ff6d3d455ffa9bf5895631d
Muhammad Habib Khan <92048010+MuhammadHabibKhan@users.noreply.github.com>
2023-12-15 22:38:06 +0500

    Add files via upload

---
+++ node-customer/Auth/index.js
@@ -0,0 +1,25 @@
+// git test: Habib
+
+
+
+const jwt = require('jsonwebtoken');
+const key = 'secret';
+
+const createToken = (customer) => {
+    const toke = jwt.sign({ email: customer.email }, key);
+    return toke;
+  };
+
+  const checkToken = (token) => {
+    try {
+      const decoded = jwt.verify(token, key);
+      return decoded;
+    } catch (err) {
+      return null;
+    }
+  }
+  
+  module.exports = {
+    createToken,
+    checkToken
+  };
